0e2cf8df1d639bb4e8a3972f316147a8c86d271d
Ramazanov Djabrail <ramazanovd612@gmail.com>
2022-11-22 15:44:28 +0600

    nnnn

---
+++ src/components/Message/NewMessage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const newMessage = () => {
+  return (
+    <div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
+
+export default newMessage
